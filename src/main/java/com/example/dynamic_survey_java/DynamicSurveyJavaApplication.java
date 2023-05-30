b233bfce7663030d6772e85b8d8c0a8f3a3175e7
peishan-Chiang <peishanchiang0816@gmail.com>
2023-05-30 16:57:12 +0800

    first commit

---
+++ src/main/java/com/example/dynamic_survey_java/DynamicSurveyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_survey_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicSurveyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicSurveyJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamic_survey_java/DynamicSurveyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_survey_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicSurveyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
